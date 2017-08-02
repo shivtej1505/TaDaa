16089ae0ecaf438d55fe2f1907eafbacdcb6140b
Shivang Nagaria <shivang.nagaria@gmail.com>
2017-08-02 10:43:32 +0530

    styling added

--- popup.js
+++ popup.js
@@ -1,19 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
+    var storage = chrome.storage.local;
     var list = document.getElementById("todo-list");
     var todos = [];
-    chrome.storage.local.get("todos", function (items) {
+    storage.get("todos", function (items) {
         console.log(items);
         todos = items.todos;
         for(var i = 0; i < todos.length; i++) {
             console.log(todos[i]);
-            var todo = document.createElement("li");
-            todo.className = "list-group-item";
-            todo.innerHTML = todos[i];
-            list.appendChild(todo);
+            addTodo(list, todos[i]);
         }
     });
 
     var addBtn = document.getElementById("add-new-todo");
+    var clearBtn = document.getElementById("clear-todos");
     var todoText = document.getElementById("new-todo-text");
 
     addBtn.addEventListener('click', function () {
@@ -22,13 +21,24 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log(todos);
         todos.push(newTodo);
 
-        var todo = document.createElement("li");
-        todo.className = "list-group-item";
-        todo.innerHTML = newTodo;
-        list.appendChild(todo);
+        addTodo(list, newTodo);
 
-        chrome.storage.local.set({"todos": todos}, function () {
+        storage.set({"todos": todos}, function () {
             console.log("new todo saved")
         });
-    })
+    });
+
+    clearBtn.addEventListener('click', function () {
+        storage.set({"todos": []});
+        while (list.firstChild) {
+            list.removeChild(list.firstChild);
+        }
+    });
 });
+
+function addTodo(list, todoText) {
+    var todo = document.createElement("li");
+    todo.className = "list-group-item";
+    todo.innerHTML = todoText;
+    list.appendChild(todo);
+}
