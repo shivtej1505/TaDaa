aa4ec3b2222daf7637655b5df872757d8226c1c1
Shivang Nagaria <shivang.nagaria@gmail.com>
2017-08-12 20:48:42 +0530

    footerApp added

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,35 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-render(<p>Hello Guys!</p>, document.getElementById('root'));
+class App extends React.Component {
+    addTodo(value) {
+        console.log("add");
+        console.log(value);
+    }
+
+    clearTodos(value){
+        console.log("clear");
+        console.log(value);
+    }
+
+    render() {
+        let todo;
+        return (
+        <div>
+            <h2>TaDaa - Simple ToDo</h2>
+            <div className="contentApp">
+                <ul className="list-group">
+                    <li>Lite</li>
+                </ul>
+            </div>
+            <div className="footerApp">
+                <input type="text" name="new-todo" ref={node => {todo = node}} />
+                <button className="btn add-btn" onClick={() => this.addTodo(todo.value)}>Add</button>
+                <button className="btn clear-btn" onClick={() => this.clearTodos(todo.value)}>Clear</button>
+            </div>
+        </div>
+    )}
+}
+
+
+render(<App/>, document.getElementById('root'));
