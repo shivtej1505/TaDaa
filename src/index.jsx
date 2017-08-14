e3d6ccf81aa4909a789a5dc4e3dd8425d4e75021
Shivang Nagaria <shivang.nagaria@gmail.com>
2017-08-14 17:52:25 +0530

    storage added

--- src/index.jsx
+++ src/index.jsx
@@ -7,11 +7,13 @@ class App extends React.Component {
     addTodo(value) {
         console.log(value);
         let { store } = this.props;
+        addTodo(value);
         store.dispatch({type: 'ADD_TODO', value: value});
     }
 
     clearTodos() {
         let { store } = this.props;
+        clearTodos();
         store.dispatch({type: 'CLEAR_TODOS'});
     }
 
@@ -46,6 +48,9 @@ const todoReducer = (state = [], action) => {
         case 'CLEAR_TODOS':
             newState = [];
             break;
+        case 'LOAD_INITIAL_TODOS':
+            newState = action.todos;
+            break;
         default:
             break;
     }
@@ -60,4 +65,23 @@ const lite = () => {
 
 store.subscribe(lite);
 
-lite();
+const storage = chrome.storage.local;
+
+storage.get("todos", function (items) {
+    console.log(items);
+    store.dispatch({type: 'LOAD_INITIAL_TODOS', todos: items.todos});
+});
+
+function addTodo(todo) {
+    let todos = [];
+    storage.get("todos", function (items) {
+        console.log(items);
+        todos = items.todos;
+        todos.push(todo);
+        storage.set({"todos": todos});
+    });
+}
+
+function clearTodos() {
+    storage.set({"todos": []});
+}
